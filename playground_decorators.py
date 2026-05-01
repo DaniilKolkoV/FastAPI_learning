b98c5355e3372e73566d0d0290a05d155156a669
Daniil <daniilkolkov001@gmail.com>
2026-05-01 23:55:32 +0300

    Update decorators playground with parameterized retry

--- playground_decorators.py
+++ playground_decorators.py
@@ -1,18 +1,37 @@
-def count_calls(func):
-    nonlocal i = 0
-    def wrapper():
-        nonlocal i = i + 1
-        result = func()
-        print(f"Функция {func.__name__}, вызвана {i} раз")
-        return result
-    return wrapper
+import random
+
+def retry(count):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for i in range(1,count + 1):
+                try:
+                    print(f"Попытка {i}:")
+                    result = func(*args, **kwargs)
+                    return result
+                except Exception as e:
+                    print(f"Попытка {i} не удалась, потому что {e}, попытка не удалась")
+                    if i == count:
+                        raise
+        return wrapper
+    return decorator
 
 
-@count_calls
-def hello():
-    print("Hello!")
+@retry(5)
+def unstable_5():
+    n = random.randint(1, 4)
+    if n != 1:
+        raise ValueError(f"Не повезло, выпало {n}")
+    return "Успех (5 попыток)!"
 
 
-hello()
-hello()
-hello()
+@retry(2)
+def unstable_2():
+    n = random.randint(1, 4)
+    if n != 1:
+        raise ValueError(f"Не повезло, выпало {n}")
+    return "Успех (2 попытки)!"
+
+
+print(unstable_5())
+print("===")
+print(unstable_2())
